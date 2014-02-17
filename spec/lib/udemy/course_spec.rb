1fd289ef55eb0c66239f2652fc4d79473e7cd0d4
Jonathan Lancar <jonaphin@gmail.com>
2014-02-16 20:20:16 -0800

    Test single course API call

--- spec/lib/udemy/course_spec.rb
+++ spec/lib/udemy/course_spec.rb
@@ -1,10 +1,24 @@
 require 'spec_helper'
 
 describe Udemy::Course do
+  before(:all) do
+    Udemy.client_key = 'abc'
+    Udemy.client_secret = '123'
+  end
 
   context 'findOne' do
+    let(:course_response) { File.read(Rails.root.join("../fixtures/course.html")) }
+
     it "should require an id" do
       expect{ Udemy::Course.findOne(nil) }.to raise_error(Udemy::Error)
+    end
+
+    it "returns an object containing a title" do
+      stub_request(:get, "https://www.udemy.com/api-1.1/courses/5816").
+               to_return(:status => 200, :body => course_response, :headers => { content_type: "html/text"})
+
+      course = Udemy::Course.findOne(5816)
+      course.title.should_not be_blank
     end
   end
 end
